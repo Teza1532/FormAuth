36f8aa0d04b3609aacfba8acbd41142a4cb3003d
Terry <Terence.Lawson1532@gmail.com>
2019-04-20 15:00:17 +0100

    Added up until Repository

---
+++ JotFormAuthenticator/Controllers/JotFormController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FormModels.Models;
+using JotFormParserModels.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JotFormAuthenticator.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JotFormController : ControllerBase
+    {
+
+        [HttpGet]
+        public String Get() => ("jotformParse");
+
+        [HttpPost]
+        public void Post([FromForm] JotFormModel Form)
+        {
+            FormModel form = new FormModel {
+                FormID = Form.FormID,
+                FormName = Form.FormTitle,
+            };
+        }
+    }
+}
---
+++ JotFormAuthenticator/DBContext/FormDBContext.cs
@@ -0,0 +1,18 @@
+﻿using FormModels.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormAuthenticator.DBContext
+{
+    public class FormDBContext : DbContext
+    {
+        public FormDBContext(DbContextOptions<FormDBContext> options) : base(options) { }
+        public DbSet<FormModel> FormModels { get; set; }
+        public DbSet<FieldModel> FieldModels { get; set; }
+        public DbSet<FormSubmission> FormSubmissions { get; set; }
+        public DbSet<SubmissionFieldValues> SubmissionFieldValues { get; set; }
+    }
+}
