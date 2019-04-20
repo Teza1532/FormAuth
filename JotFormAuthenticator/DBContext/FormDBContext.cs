203372efbf88ed8d59c0cc41ed21ac48e6e37cbf
Terry <Terence.Lawson1532@gmail.com>
2019-04-20 15:13:59 +0100

    added the functionality to SaveChanges to add created on to the date

--- JotFormAuthenticator/DBContext/FormDBContext.cs
+++ JotFormAuthenticator/DBContext/FormDBContext.cs
@@ -14,5 +14,27 @@ namespace FormAuthenticator.DBContext
         public DbSet<FieldModel> FieldModels { get; set; }
         public DbSet<FormSubmission> FormSubmissions { get; set; }
         public DbSet<SubmissionFieldValues> SubmissionFieldValues { get; set; }
+
+
+        public override int SaveChanges()
+        {
+            var AddedEntities = ChangeTracker.Entries().Where(E => E.State == EntityState.Added).ToList();
+
+            AddedEntities.ForEach(E =>
+            {
+                E.Property("CreatedOn").CurrentValue = DateTime.Now;
+            });
+
+            var EditedEntities = ChangeTracker.Entries().Where(E => E.State == EntityState.Modified).ToList();
+
+            EditedEntities.ForEach(E =>
+            {
+                E.Property("UpdatedOn").CurrentValue = DateTime.Now;
+            });
+
+            return base.SaveChanges();
+        }
     }
+
 }
+
